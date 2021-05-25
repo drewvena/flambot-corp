174ca9aa274a444feaa389afb48059b004fd7007
Drew Vena <drewvena1@gmail.com>
2021-05-24 21:15:10 -0500

    seeding

---
+++ server/config/seeds.js
@@ -0,0 +1,48 @@
+const db = require('./connection');
+const { User, Product, Category } = require('../models');
+
+db.once('open', async () => {
+  await Category.deleteMany();
+
+  const categories = await Category.insertMany([
+    { name: 'Crew Neck Tee' },
+    { name: 'Long Sleeve Tee' },
+    { name: 'Black Shirts' },
+    { name: 'White Shirts' },
+    { name: 'Graphic Tees' }
+  ]);
+
+  console.log('categories seeded');
+
+  await Product.deleteMany();
+
+  const products = await Product.insertMany([
+    {
+      name: 'Black Shirt',
+      description:
+        'Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos.',
+      image: 'cookie-tin.jpg',
+      category: categories[0]._id,
+      price: 15.99,
+      quantity: 5
+    }
+  ]);
+
+  console.log('products seeded');
+
+  await User.deleteMany();
+
+  await User.create({
+    firstName: 'Drew',
+    lastName: 'Vena',
+    email: 'Drewvena1@gmail.com',
+    password: 'imsweet66',
+    orders: [
+      
+    ]
+  });
+
+  console.log('users seeded');
+
+  process.exit();
+});
