1d586ddec6a6bbdefe9c0b753abe55960ea678a1
Drew Vena <drewvena1@gmail.com>
2021-05-24 20:47:05 -0500

    typedefs done

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,43 @@
+const {gql} =  require('apollo-server-express');
+
+const typeDefs = gql `
+type User {
+    _id: ID
+    firstName: String
+    lastName: String
+    email: String
+    orders: [Order]
+}
+type Order {
+    _id: ID
+    purchaseDate: String
+    products: [Product]
+}
+type Auth {
+    token: ID
+    user: User
+}
+type Category {
+    _id: ID
+    name: String
+}
+type Checkout {
+    session: ID
+}
+type Query {
+    categories: [Category]
+    products(category: ID, name: String): [Product]
+    product(_id: ID!): Product
+    user: User
+    order(_id: ID!): Order
+    checkout(products: [ID]!): Checkout
+}
+type Mutation {
+    addUser(firstName: String!, lastName: String!, email: String!, password: String!): Auth
+    addOrder(products: [ID]!): Order
+    updateUser(firstName: String, lastName: String, email: String, password: String): User
+    updateProduct(_id: ID!, quantity: Int!): Product
+    login(email: String!, password: String!): Auth
+}
+`
+module.exports = typeDefs;
