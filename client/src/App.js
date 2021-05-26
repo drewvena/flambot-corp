a55a0cfeb903baaec6bc87db13f93b766807283c
Raymond Lopez <raymondlopez13@yahoo.com>
2021-05-25 19:17:45 -0500

    home page says hello

--- client/src/App.js
+++ client/src/App.js
@@ -1,17 +1,13 @@
 import React, { useEffect } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import { ApolloProvider } from '@apollo/react-hooks';
-import { useQuery } from '@apollo/react-hooks';
-import { QUERY_PRODUCTS } from "./utils/queries";
-import { UPDATE_PRODUCTS } from "./utils/actions";
 import ApolloClient from 'apollo-boost';
-// import Home from "./pages/Home";
+import Home from "./pages/Home";
 // import Detail from "./pages/Detail";
 // import NoMatch from "./pages/NoMatch";
 // import Login from "./pages/Login";
 // import Signup from "./pages/Signup";
 // import Nav from "./components/Nav";
-import { StoreProvider, useStoreContext } from "./utils/GlobalState";
 // import Success from "./pages/Success";
 // import OrderHistory from "./pages/OrderHistory";
 // import { idbPromise } from './utils/helpers';
@@ -24,12 +20,12 @@ const client = new ApolloClient({
       }
     })
   },
-  uri: 'localhost:3001/graphql',
+  uri: 'https://localhost:3001/graphql',
 });
 function App() {
   return (
     <ApolloProvider client={client}>
-      <h2>Hello</h2>
+      <Home />
     </ApolloProvider>
   );
 }
---
+++ client/src/pages/Home.js
@@ -0,0 +1,16 @@
+import React, { useState, useEffect } from "react";
+import { QUERY_ALL_PRODUCTS } from '../utils/queries';
+import { useQuery } from '@apollo/react-hooks';
+import { useStoreContext } from '../utils/GlobalState';
+
+
+const Home = () => {
+  
+  return (
+  <div className="container">
+      <h2>Hello</h2>
+  </div>
+  );
+};
+
+export default Home;
