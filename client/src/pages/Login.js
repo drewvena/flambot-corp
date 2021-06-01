b2b651c620fb5060d7110f3bc80a9144927bc1b0
Cody Kimb <codykimbell@gmail.com>
2021-06-01 11:50:33 -0500

    login

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,5 +1,8 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from 'react-router-dom';
+import { useMutation } from '@apollo/react-hooks';
+import { LOGIN } from "../utils/mutations"
+import Auth from "../utils/auth";
 
 const Login = () => {
     function showPass() {
@@ -10,14 +13,37 @@ const Login = () => {
             p.type ='password';
         }
     }
+
+    const [formState, setFormState] = useState({ email: '', password: '' })
+    const [login, { error }] = useMutation(LOGIN);
+
+    const handleFormSubmit = async event => {
+        event.preventDefault();
+        try {
+        const mutationResponse = await login({ variables: { email: formState.email, password: formState.password } })
+        const token = mutationResponse.data.login.token;
+        Auth.login(token);
+        } catch (e) {
+        console.log(e)
+        }
+    };
+
+    const handleChange = event => {
+        const { name, value } = event.target;
+        setFormState({
+        ...formState,
+        [name]: value
+        });
+    };
+
     return (
         <div className="container">
             <h2>Login!</h2>
-            <form>
+            <form onSubmit={handleFormSubmit}>
                 <label htmlFor="email">Email: </label>
-                <input type='text' name='text' placeholder="email@gmail.com"></input>
+                <input type='email' name='email' placeholder="email@gmail.com" onChange={handleChange}></input>
                 <label htmlFor='password'>Password: </label>
-                <input type='password' name='password' id='pass'></input>
+                <input type='password' name='password' id='pass' onChange={handleChange}></input>
                 <label htmlFor='check'>Show password</label>
                 <input type='checkbox' name='check' onClick={showPass}></input>
                 <button type='submit'>Submit</button>
