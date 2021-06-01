059a848a1d32b4eddd104d19d57cfaed43479419
Raymond Lopez <raymondlopez13@yahoo.com>
2021-06-01 11:40:43 -0500

    signup working

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -1,7 +1,25 @@
-import React from "react";
+import React, {useState} from "react";
 import { Link } from 'react-router-dom';
+import { useMutation } from '@apollo/react-hooks';
+import Auth from "../utils/auth";
+import { ADD_USER } from "../utils/mutations";
 
-const Login = () => {
+const Signup = () => {
+    const [formState, setFormState] = useState({ email: '', password: '' });
+    const [addUser] = useMutation(ADD_USER);
+
+    const handleFormSubmit = async event => {
+        event.preventDefault();
+        const mutationResponse = await addUser({
+          variables: {
+            email: formState.email, password: formState.password,
+            firstName: formState.firstName, lastName: formState.lastName
+          }
+        });
+        const token = mutationResponse.data.addUser.token;
+        Auth.login(token);
+      };
+
     function showPass() {
         var p = document.getElementById('pass');
         if(p.type === 'password') {
@@ -10,16 +28,33 @@ const Login = () => {
             p.type ='password';
         }
     }
+
+    function handleChange(event) {
+        const { name, value } = event.target;
+        setFormState({
+            ...formState,
+            [name]: value
+        });
+    }
+
     return (
         <div className="container">
             <h2>Sign up!</h2>
-            <form>
+            <form onSubmit={handleFormSubmit}>
+                <label htmlFor="firstName">First Name: </label>
+                <input type='text' name='firstName' placeholder="Your First Name" id='firstName' onChange={handleChange}></input>
+
+                <label htmlFor="lastName">Last Name: </label>
+                <input type='text' name='lastName' placeholder="Your Last Name" id='lastName' onChange={handleChange}></input>
+
                 <label htmlFor="email">Email: </label>
-                <input type='text' name='text' placeholder="email@gmail.com"></input>
+                <input type='email' name='email' placeholder="email@gmail.com" id='email' onChange={handleChange}></input>
+
                 <label htmlFor='password'>Password: </label>
-                <input type='password' name='password' id='pass'></input>
-                <label htmlFor='check'>Show password</label>
+                <input type='password' name='password' id='pass' onChange={handleChange}></input>
+                <label htmlFor='check' id='password'>Show password</label>
                 <input type='checkbox' name='check' onClick={showPass}></input>
+
                 <button type='submit'>Submit</button>
                 <h3>Or, <Link to='/login'>click here to login</Link></h3>
             </form>
@@ -27,4 +62,4 @@ const Login = () => {
     );
 };
 
-export default Login;
+export default Signup;
