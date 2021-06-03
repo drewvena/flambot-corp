8a47d35c359928d13ff580f7a6c9dd38fee6f1d5
Cody Kimb <codykimbell@gmail.com>
2021-06-03 14:31:51 -0500

    deleted comments on signup

--- client/src/pages/Signup.js
+++ client/src/pages/Signup.js
@@ -42,31 +42,14 @@ const Signup = () => {
         <div className="container my-20">
             <Header as="h1">Sign up!</Header>
             <Form onSubmit={handleFormSubmit}>
-                {/* <label htmlFor="firstName">First Name: </label>
-                <input type='text' name='firstName' placeholder="Your First Name" id='firstName' onChange={handleChange}></input> */}
                 <Form.Field control="input" htmlFor="firstName" type='text' name='firstName' label="First Name"placeholder="Your First Name" id='firstName' onChange={handleChange} />
-
-                {/* <label htmlFor="lastName">Last Name: </label>
-                <input type='text' name='lastName' placeholder="Your Last Name" id='lastName' onChange={handleChange}></input> */}
                 <Form.Field control="input" htmlFor="lastName" type='text' name='lastName' label="Last Name" placeholder="Your Last Name" id='lastName' onChange={handleChange} />
-
-                {/* <label htmlFor="email">Email: </label>
-                <input type='email' name='email' placeholder="email@gmail.com" id='email' onChange={handleChange}></input> */}
                 <Form.Field control= {Input} htmlFor="email" type='email' name='email' label="Email" placeholder="email@gmail.com" onChange={handleChange}/>
-                
-                {/* <label htmlFor='password'>Password: </label>
-                <input type='password' name='password' id='pass' onChange={handleChange}></input> */}
                 <Form.Field control= {Input} htmlFor='password' type='password' name='password' label="Password" id='pass' onChange={handleChange}/>
-
-                {/* <label htmlFor='check' id='password'>Show password</label>
-                <input type='checkbox' name='check' onClick={showPass}></input>
-
-                <button type='submit'>Submit</button> */}
                 <Form.Group widths="equal">
                     <Form.Field control={Button} primary type='submit'>Submit</Form.Field>
                     <Form.Field control = {Checkbox} htmlFor='check' label="Show password" onChange={showPass}/>
                 </Form.Group>
-
                 <h3>Or, <Link to='/login'>click here to login</Link></h3>
             </Form>
         </div>
