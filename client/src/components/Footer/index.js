8c95c00dc6fce654037bcb9adf2288013152ce21
Julissa Hash <69886471+yoyachi@users.noreply.github.com>
2021-06-04 01:26:10 -0500

    Added coupon div to footer

--- client/src/components/Footer/index.js
+++ client/src/components/Footer/index.js
@@ -4,6 +4,14 @@ import { Menu, Grid} from 'semantic-ui-react'
 
 function Footer() {
     return (
+      <>
+
+        <div style={{backgroundColor:'yellow', height:'20vh', textAlign:'center'}}>
+
+              <p style={{paddingTop:'30px' }}>Get a 15% discount coupon CODE: F1412</p>
+              
+
+        </div>
         <Menu horizontal link inverted  size='small' widths = {5} >
             
             <Menu.Item as='a'>
@@ -23,6 +31,7 @@ function Footer() {
             </Menu.Item>
 
         </Menu>
+        </>
       );
 }
 
